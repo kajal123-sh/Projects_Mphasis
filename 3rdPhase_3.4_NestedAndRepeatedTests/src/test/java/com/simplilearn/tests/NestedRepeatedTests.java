d04908e8666823b142f2410aa70a0ad311a17589
kajal123-sh <shahanekajal023@gmail.comgit config --global user.name kajal123-sh>
2022-03-29 11:03:49 +0000

    added changes

---
+++ 3rdPhase_3.4_NestedAndRepeatedTests/src/test/java/com/simplilearn/tests/NestedRepeatedTests.java
@@ -0,0 +1,51 @@
+package com.simplilearn.tests;
+
+import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+public class NestedRepeatedTests {
+
+	@BeforeEach
+	void beforeEach() {
+		System.out.println("Before Each of Demo5 (Outer class)");
+	}
+	
+	@AfterEach
+	void afterEach() {
+		System.out.println("After Each of Demo5(Outer class)");
+	}
+	
+	
+	@BeforeAll
+	static void beforeAll() {
+		System.out.println("Before All  of Demo5 (Outer class)");
+	}
+	
+	@Nested
+	class A {
+		
+		@BeforeEach
+		void beforeEach() {
+			System.out.println("Before Each of A");
+		}
+		
+		@Test
+		void testSampleMEthod() {
+			System.out.println("Test executed from Nested inner class");
+		}
+		
+		@AfterEach
+		void afterEach() {
+			System.out.println("After Each of A");
+		}
+	}
+
+}
+
+
